d79a5837f1403dc8582b49940bdccee8d112a69f
Dickens Kongoro <36263515+FrankDickenz@users.noreply.github.com>
2023-06-21 07:51:43 +0000

    Projects Fix

--- erpnext/projects/doctype/project/project_dashboard.py
+++ erpnext/projects/doctype/project/project_dashboard.py
@@ -11,8 +11,8 @@ def get_data():
 				"label": _("Project"),
 				"items": ["Task", "Timesheet", "Issue", "Project Update"],
 			},
-			{"label": _("Material"), "items": ["Material Request", "BOM", "Stock Entry"]},
-			{"label": _("Sales"), "items": ["Sales Order", "Delivery Note", "Sales Invoice"]},
-			{"label": _("Purchase"), "items": ["Purchase Order", "Purchase Receipt", "Purchase Invoice"]},
+			# {"label": _("Material"), "items": ["Material Request", "BOM", "Stock Entry"]},
+			# {"label": _("Sales"), "items": ["Sales Order", "Delivery Note", "Sales Invoice"]},
+			# {"label": _("Purchase"), "items": ["Purchase Order", "Purchase Receipt", "Purchase Invoice"]},
 		],
 	}
